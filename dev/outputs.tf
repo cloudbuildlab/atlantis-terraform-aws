0fa9c4cb1d8894256fd8383bcc4045638c9de706
John Ajera <j.ajera@gns.cri.nz>
2026-01-28 14:18:46 +1300

    chore: ad output
    adds s3 bucket arn output

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -2,8 +2,3 @@ output "bucket_name" {
   description = "Name of the created S3 bucket"
   value       = data.aws_s3_bucket.example.id
 }
-
-output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = data.aws_s3_bucket.example.arn
-}
